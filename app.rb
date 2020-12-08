7b3bf7b3dc0748f58f9eb8f08a6c5989fd7c5b53
xavierloos <xavier.loos04@gmail.com>
2020-12-08 18:54:43 +0000

    Updating form as the names didn't show up on sending

--- app.rb
+++ app.rb
@@ -2,20 +2,12 @@ require "sinatra/base"
 
 class Battle < Sinatra::Base
   get "/" do
-    "Testing infrastructure working!"
-  end
-
-  get '/player' do
     erb :index
   end
 
-  post '/names' do
-    p params
+  post "/names" do
     @player_1 = params[:player_1]
     @player_2 = params[:player_2]
     erb :names
   end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,14 +1,9 @@
-feature "Testing infrastructure" do
-  scenario "Can run app and check page content" do
+feature "enter names" do
+  scenario "submitting names" do
     visit("/")
-    expect(page).to have_content "Testing infrastructure working!"
-  end
-  scenario "expects players to submit names which appear on the page content" do
-    visit("/players")
-    fill_in :player_1, :with => "Ian"
-    fill_in :player_2, :with => "Javi"
-    click_button "submit"
-
-    expect(page).to have_text("Ian Vs. Javi")
+    fill_in :player_1, with: "Ian"
+    fill_in :player_2, with: "Javi"
+    click_button "Submit"
+    expect(page).to have_content "Ian vs. Javi"
   end
 end
